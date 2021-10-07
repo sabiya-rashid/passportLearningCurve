53219c85c2d2390992c7aa12ed64e69f8fdbb343
sabiya-rashid <bhatsabiya2@gmail.com>
2021-10-07 15:20:05 +0530

    logout, auth added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const {
+    ensureAuthenticated
+} = require('../config/auth')
 
 //Welcome page
 router.get('/', (req, res) => res.render('welcome'))
 
 //Dashboard page
-router.get('/dashboard', (req, res) => res.render('dashboard'))
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+    name: req.user.name
+}))
 
 module.exports = router
